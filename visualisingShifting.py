66002ebf528c0b43867415cf9fe2b59c33c4f60b
Hacker1337 <amirfvb@gmil.com>
2020-04-01 23:01:20 +0300

    Program, which draws two graphics with a slight shifting is added.

---
+++ visualisingShifting.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+firstN, secondN = (170, 181)
+shifting = 136
+
+data1 = np.loadtxt("данные для обработки/WFM" + str(firstN) + ".csv", skiprows=1, delimiter=',')
+data2 = np.loadtxt("данные для обработки/WFM" + str(secondN) + ".csv", skiprows=1, delimiter=',')
+
+plt.plot((np.arange(len(data1))), data1[:, 2])
+plt.plot((np.arange(len(data2)) + shifting), data2[:, 2])
+# plt.plot((np.arange(len(data1)) + 470) % len(data1), data1[:, 1])
+plt.show()
